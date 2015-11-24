c28ae6c40e24bb272ec29e7d8e6016284b6f97e4
mtwohig <mtwohigcork@gmail.com>
2015-11-24 02:46:02 +0000

    First few exercises

---
+++ Keywords2.java
@@ -0,0 +1,9 @@
+/* 
+ * Exercise 1a
+*/
+public class Keywords2 {
+    public static void main(String[] args) {
+        System.out.println("Keyword");
+	System.out.println("Testing");
+    }
+}
---
+++ Operators.java
@@ -0,0 +1,12 @@
+/* 
+ * Exercise 1
+*/
+public class Operators {
+    public static void main(String[] args) {
+        int x = 1000;
+	int y = 35;
+	int result = x / y;
+	x++;
+	System.out.println("x = " + x );
+    }
+}
